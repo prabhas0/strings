8401b453f0a029ecf420370c81c1da3598eb5b93
Rohit kumar <9rohitman@gmail.com>
2024-06-18 19:08:34 +0530

    reverse

---
+++ reverese.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	string s;
+	getline(cin,s);
+	int n=s.length();
+	int i=n/2;
+	int j=n-1;
+	while(i<j){
+         swap(s[i],s[j]);
+		 i++;
+		 j--;
+	}
+	cout<<s;
+
+}
